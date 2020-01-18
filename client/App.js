3c80ddd7d9db1291c2ea67306f033d221da38e70
RoyOlivarez <royolivarez66@gmail.com>
2020-01-18 12:33:21 -0600

    Create React App file, and connected the server.

--- client/App.js
+++ client/App.js
@@ -1,1 +1,20 @@
+import React from 'react';  // - - > imports React library
 
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+  };
+
+  render() {
+    return (
+      <div>
+        <h4>React page is working</h4>
+      </div>
+    )
+  }
+};  

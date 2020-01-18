5fe4b614b5b6b6af528e9b2644094755f41ed0aa
RoyOlivarez <royolivarez66@gmail.com>
2020-01-18 13:57:51 -0600

    Added input bar and submit button. Page also has a title

--- client/App.js
+++ client/App.js
@@ -1,19 +1,21 @@
-import React from 'react';  // - - > imports React library
-
-
+import React from 'react';  // --> imports React library
+import axios from 'axios'; // --> imports axios library
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      todos: []
+    this.state = { 
+      tasks: [] // --> This is the initial state
     };
   };
 
   render() {
     return (
+      
       <div>
-        <h4>React page is working</h4>
+        <h4>Roy's Task List </h4>
+        <input type="text"></input> 
+        <button>Add Task</button>
       </div>
     )
   }

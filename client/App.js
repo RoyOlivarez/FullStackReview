69336bc7849597bded0c93ba3e0870e5486f3ef9
RoyOlivarez <royolivarez66@gmail.com>
2020-04-12 17:01:22 -0500

    initial App setup

--- client/App.js
+++ client/App.js
@@ -1,1 +1,16 @@
+import React from 'react';
 
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {};
+  }
+}
+
+render () {
+  return (
+    <div>
+      <h1>This is React!!!!</h1>
+    </div>
+  )
+}

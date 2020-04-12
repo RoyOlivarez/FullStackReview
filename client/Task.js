90f402096d85ea65126b8167160da17d32b3e192
RoyOlivarez <royolivarez66@gmail.com>
2020-04-12 16:17:54 -0500

    updated to restart fullstack recview for OH's

--- client/Task.js
+++ client/Task.js
@@ -1,24 +1,2 @@
-import React from 'react';
 
-// Notes: task in map is the prop that you created in the Task component on App.
-const Task = (props) => {
-  return (
-    <div>
-        {props.task.map((item, index) => ( //make sure you add props to Task in App
-          <li key={index}>{item.task}</li>
-        ))}
-    </div>
-  )
-};
-
-// const Task = props => (
-//   <div key={`task_${props.id}`}>
-//     <span>{props.task}</span>
-//     <button value={props.id} onClick={props.deleteTask}>
-//       Complete
-//     </button>
-//   </div>
-// );
-
-export default Task;
 

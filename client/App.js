b99fb55d0305ea00acd0cc356c2ef1e3fc46c89c
RoyOlivarez <royolivarez66@gmail.com>
2020-04-12 23:49:23 -0500

    added post/get route and input funtionality

--- client/App.js
+++ client/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -7,14 +8,54 @@ export default class App extends React.Component {
       inputTask: '',
       tasks: []
     };
+    this.sendTask = this.sendTask.bind(this);
+    this.getTasks = this.getTasks.bind(this);
+    this.taskInput = this.taskInput.bind(this);
+  }
+
+  componentDidMount() {
+    this.getTasks();
+  }
+
+  getTasks() {
+    axios
+      .get('/todo')
+      .then(response => {
+        console.log('This is from app get request: ', response);
+        this.setState({
+          tasks: response.data
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  sendTask() {
+    axios
+      .post('/todo', {
+        task: this.state.inputTask
+      })
+      .then(() => {
+        this.setState({
+          inputTask: ''
+        });
+        return this.getTasks();
+      });
+  }
+
+  taskInput(event) {
+    this.setState({
+      inputTask: event.target.value
+    });
   }
 
   render() {
     return (
       <div>
         <h3>Todo list</h3>
-        <input />
-        <button>Add Task</button>
+        <input value={this.state.inputTask} onChange={this.taskInput} />
+        <button onClick={this.sendTask}>Add Task</button>
       </div>
     );
   }
